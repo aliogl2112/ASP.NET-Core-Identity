2d41368409db294cc685cea280754d9a2e7777f6
aliogl2112 <you@aliogul2112@gmail.com>
2024-05-01 20:46:10 +0300

    yeni kullanici ekleme

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Identity.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Identity.Controllers
@@ -10,9 +11,43 @@ namespace Identity.Controllers
         {
             _userManager=userManager;
         }
+        [HttpGet]
         public IActionResult Index()
         {
             return View(_userManager.Users); //tüm user bilgilerini (kullanıcı listesini) alıyoruz
         }
-    }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+		[HttpPost]
+		public async Task<IActionResult> Create(CreateViewModel model)
+		{
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+
+                    UserName = model.UserName,
+                    Email = model.Email
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(user,model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach(IdentityError err in result.Errors)
+                {
+                    ModelState.AddModelError("",err.Description); //hataları modele ekledik ve cshtml üzerinde gösterilecek. "" olan yere hangi state ile ilişkilendirmek istediğimizi yazıyoruz (emaili, password)
+                }
+            }
+			return View(model);
+		}
+	}
 }
---
+++ ViewModels/CreateViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.ViewModels
+{
+	public class CreateViewModel
+	{
+		[Required]
+		public string UserName { get; set; } = string.Empty;
+
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; } = string.Empty;
+
+		[Required]
+		[DataType(DataType.Password)]
+		[MinLength(8)]
+		public string Password { get; set; } = string.Empty;
+
+		[Required]
+		[DataType(DataType.Password)]
+		[Compare(nameof(Password),ErrorMessage ="Parola bilgileri eşleşmiyor.")] //Password değeri ile karşılaştırılmasını sağladık
+		public string ConfirmPassword { get; set; } = string.Empty;
+	}
+}
